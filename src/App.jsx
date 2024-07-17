9eb5b381361e6e5d0ae6a7439acce66bcd04fa64
Samhitha P <samhitha.p@planview.com>
2024-07-18 00:19:57 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
-      <div><h2>This is the main branch by samhitha </h2> </div>
+      <div><h2>This is the branch by samhitha </h2> </div>
       <Counter />
     </div>
   );
