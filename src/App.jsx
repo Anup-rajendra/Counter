40f180239e1d940650234c4fa69cd834b86df0ca
Guruprasad Raikar <gp.raikar@planview.com>
2024-07-18 00:08:19 +0530

    guru updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
-      <div>This is the main branch</div>
+      <div>This is branch_guru 
+        <h2>Updated by Guru</h2>
+      </div>
       <Counter />
     </div>
   );
